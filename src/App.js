513dc36b742a64e7a757075b5969fab41f0bca4e
username <user_khirade36@gmal.com>
2024-09-12 14:05:36 +0530

    Refactor CSS styles and layout in App component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import TabComponent from './TabComponent';
+import GalleryComponent from './GalleryComponent';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-container">
+      <div className="right-side">
+        <TabComponent />
+        <GalleryComponent />
+      </div>
     </div>
   );
 }
